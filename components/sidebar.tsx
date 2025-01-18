077f708a39662d5dbc9cb998905befb0d539d0bd
codeInterpreter05 <sarthupadhyay0507@gmail.com>
2025-01-18 18:11:14 +0530

    sidebar changes

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,7 +2,7 @@
 
 import { cn } from "@/lib/utils"
 import { console } from "inspector"
-import { CodeIcon, ImageIcon, LayoutDashboard, MessageSquare, MusicIcon, Settings, VideoIcon, SproutIcon,  } from "lucide-react"
+import { Bot, Sprout, LayoutDashboard, ChartNoAxesColumn, MusicIcon, Settings, VideoIcon, SproutIcon,  } from "lucide-react"
 import { Montserrat } from "next/font/google"
 import Image from "next/image"
 import Link from "next/link"
@@ -19,27 +19,27 @@ const routes = [
     },
     {
         label: "Chatbot",
-        icon: MessageSquare,
-        href: "/conversation",
+        icon: Bot,
+        href: "/chatbot",
         color: "text-violet-500"
     },
     {
         label: "Crops",
-        icon: ImageIcon,
-        href: "/image",
-        color: "text-pink-700"
+        icon: Sprout,
+        href: "/crops",
+        color: "text-emerald-500"
     },
     {
         label: "Analysis",
-        icon: VideoIcon,
-        href: "/video",
+        icon: ChartNoAxesColumn,
+        href: "/analysis",
         color: "text-orange-700"
     },
     {
-        label: "Cattle",
+        label: "Cattles",
         icon: MusicIcon,
-        href: "/music",
-        color: "text-emerald-500"
+        href: "/cattles",
+        color: "text-pink-700"
     },
 
 ]
@@ -54,7 +54,7 @@ const Sidebar = () => {
                 <div className="relative w-12 h-12 mr-2">
                     <Image fill alt="logo" src="/logo.png" />
                 </div>
-                <h1 className={cn("text-3xl font-bold", montserrat.className)}>
+                <h1 className={cn("text-2xl font-bold", montserrat.className)}>
                     AgroAid
                 </h1>
             </Link>
